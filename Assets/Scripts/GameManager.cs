8befc33665be06f800a5c227761b2f7f25840a03
Krish Garg <10101krish@gmail.com>
2023-10-10 03:14:12 +0530

    Base Game Done
    Base Game same to Zigurous completed. Now onto extra features.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,59 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     private int score;
 
+    public Text scoreText;
+    public GameObject playButton;
+    public GameObject gameOver;
+
+    public Player player;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+        Pause();
+    }
+
+    public void Play()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+
+        playButton.SetActive(false);
+        gameOver.SetActive(false);
+
+        Time.timeScale = 1;
+        player.enabled = true;
+
+        DestroyAllPipes();
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false;
+    }
+
     public void GameOver()
     {
-        Debug.Log("Game Over");
+        gameOver.SetActive(true);
+        playButton.SetActive(true);
+        Pause();
     }
 
     public void IncreaseScore()
     {
         score++;
+        scoreText.text = score.ToString();
+    }
+
+    private void DestroyAllPipes()
+    {
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+        for (int i = 0; i < pipes.Length; i++)
+            Destroy(pipes[i].gameObject);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -23,6 +23,12 @@ public class Player : MonoBehaviour
         InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
     }
 
+    private void OnEnable()
+    {
+        transform.position = Vector3.zero;
+        direction = Vector3.zero;
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetMouseButtonDown(0))
